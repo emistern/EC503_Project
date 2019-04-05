a8cb1a958f75d1425dc4123b98c8b5e4de53e5e1
Emily Stern <emistern@bu.edu>
2019-04-04 21:42:54 -0400

    Implemented RANSAC

--- ransac/fit_a_line.m
+++ ransac/fit_a_line.m
@@ -1,4 +1,4 @@
 function [m, b] = fit_a_line(v1, v2)
-
+% v1 and v2 are two points [x1, y1] and [x2, y2]
 m = (v1(2)-v2(2))/(v1(1)-v2(1));
 b = v1(2) - m*v1(1);
--- ransac/implement_ransac_eas.m
+++ ransac/implement_ransac_eas.m
@@ -8,69 +8,40 @@
 %% Create the synthetic Data %%
 % Set the fake data
 m = 3;
-b = 8;
+b = 2;
+Nb_samples = 1000;
 
-X = linspace(0, 100, 200);
-y = m*X + b;
-X = [X, 101];
-y = [y, 361];
+X=linspace(0,1000, Nb_samples);
 
+y=m.*X+b;
+for i=1:Nb_samples
+    noisei=randi([-abs(m*100),abs(m*100)]);
+    y(i)=(noisei)+y(i);
+    noise=rand(1);
+    y(i)=y(i)-(noise.*y(i));
+end
 
-% data_unshuffled = [X y];
-% 
-% shuffled = data_unshuffled(randperm(size(data_unshuffled,1)),:);
-% 
-% X_shuff = shuffled(:,1:end-1);
-% y_shuff = shuffled(:,end);
-% 
-% % Split the Data into training (80%) and test (20%) data
-% train_size = round(0.8*size(X_shuff,1));
-% train_x = X_shuff(1:train_size, :);
-% train_y = y_shuff(1:train_size, :);
-% test_x = X_shuff(train_size+1:end, :);
-% test_y = y_shuff(train_size+1:end, :);
 [~, nb_samples] = size(X);
 
 %% Plot the data to visualize it
 figure
 plot(X, y, 'ro')
 hold on
-plot(X, 3*X+32, 'bo')
-plot(X, 3*X-16, 'co')
 
 %% Implement RANSAC %%
 
 % RANSAC Parameters
-p = 0.99; % Probability that you have at least one inlier in your data
-u = .33; % Probability that a point is an outlier
-N_iters = ceil(nb_samples/6); % Number of iterations to do
+% p = 0.99; % Probability that you have at least one inlier in your data
+% u = .33; % Probability that a point is an outlier
+Nb_iters = ceil(nb_samples/6); % Number of iterations to do
 d_threshold = 10;
 
-% Sample two points Randomly
-samples = randi(nb_samples, 2,1);
+[all_combos] = ransac_eas(X, y, d_threshold, Nb_iters);
 
-% Sample Points
-v1 = [X(samples(1)), y(samples(1))];
-v2 = [X(samples(2)), y(samples(2))];
-% Fit a line to those two points
-[slope, b] = fit_a_line(v1, v2);
-plot(X(samples(1)), y(samples(1)), 'b*')
-plot(X(samples(2)), y(samples(2)), 'b*')
+% Get the best performing points
+[~, best_index] = max(all_combos(:,5));
+v1_best = all_combos(best_index, 1:2);
+v2_best = all_combos(best_index, 3:4);
 
-number_inliers = 0;
-
-% Count number of inliers
-for a_sample = 1:nb_samples
-    pt = [X(a_sample), y(a_sample)];
-    distance = point_to_line_dist(pt, v1, v2);
-    if distance < d_threshold
-        number_inliers = number_inliers + 1;
-    end
-end
-
-% Questions about RANSAC
-% - how to choose the threshold, should you look at the data first? maybe
-%   if you can't visualize it you can look at the std for a few different
-%   sample sets and start from there?
-
-
+[slope, b] = fit_a_line(v1_best, v2_best);
+refline(slope, b)
