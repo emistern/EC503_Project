e5bb453b4603f3067f0f8d2979b4a6b22634e6c8
Daniel Wilson <danielcw@bu.edu>
2019-04-09 23:46:51 -0400

    Get rid of the manual loop in Theil-Sen

--- theil_sen_fit.m
+++ theil_sen_fit.m
@@ -1,11 +1,9 @@
 function [m, b] = theil_sen_fit(Xtr, ytr)
 %THEIL_SEN_FIT Use the Theil-Sen algorithm for regression.
 %   Uses the median of slopes between any 2 points
-    n_samples = size(Xtr, 1);
-    slopes=zeros(n_samples, n_samples);
-    for idx=1:n_samples
-        slopes(idx, idx+1:end) = (ytr(idx+1:end) - ytr(idx)) ./ (Xtr(idx+1:end, 1) - Xtr(idx, :));
-    end
+    ydiffs = pdist2(ytr, ytr, @(a, b) b-a);
+    xdiffs = pdist2(Xtr, Xtr, @(a, b) b-a);
+    slopes = ydiffs ./ xdiffs;
     
     % Only use the upper right part of the matrix so we ignore duplicates
     % from symmetric points, and avoid self-comparisons on the diagonal.
