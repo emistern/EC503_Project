9b181ac1dad8778418bb6563ff8dcadc4354dfc7
Daniel Wilson <danielcw@bu.edu>
2019-04-10 00:13:01 -0400

    Make the lines stand out more in the scatterplot

--- test_algorithms.m
+++ test_algorithms.m
@@ -4,15 +4,15 @@ run_test(@make_noisy_data3, 3, 1);
 run_test(@make_noisy_data4, 3, 1);
 run_test(@make_noisy_data5, 3, 1);
 run_test(@make_noisy_data6, 3, 1);
-run_test(@(m, b) make_noisy_data7(m, b, [10 100], [300 400], 0.2), 3, 1);
+run_test(@(m, b) make_noisy_data7(m, b, [10 100], [600 800], 0.48), 3, 1);
 
 function run_test(f, m1, b1)
     [Xtr ytr is_outlier] = f(m1, b1);
     
     figure();
     hold on;
-    scatter(Xtr(~is_outlier), ytr(~is_outlier), 'b');
-    scatter(Xtr(is_outlier), ytr(is_outlier), 'r');
+    scatter(Xtr(~is_outlier), ytr(~is_outlier), 4, 'k');
+    scatter(Xtr(is_outlier), ytr(is_outlier), 4, [.8 .8 .8]);
     
     [m, b] = repeated_median_fit(Xtr, ytr);
     rmf=refline(m, b);
--- theil_sen_fit.m
+++ theil_sen_fit.m
@@ -1,8 +1,8 @@
 function [m, b] = theil_sen_fit(Xtr, ytr)
 %THEIL_SEN_FIT Use the Theil-Sen algorithm for regression.
 %   Uses the median of slopes between any 2 points
-    ydiffs = pdist2(ytr, ytr, @(a, b) b-a);
-    xdiffs = pdist2(Xtr, Xtr, @(a, b) b-a);
+    ydiffs = pdist2(ytr, ytr, @minus);
+    xdiffs = pdist2(Xtr, Xtr, @minus);
     slopes = ydiffs ./ xdiffs;
     
     % Only use the upper right part of the matrix so we ignore duplicates
