7c41a55dd8dc40dfe53c7814961d9bcc0307a58a
Kate Levinson <Kate@dhcp-wifi-8021x-168-122-207-213.bu.edu>
2019-04-25 17:41:48 -0400

    Changed ransac algorithm in high level directory to handle multi dimensions

--- all_pts_to_line.m
+++ all_pts_to_line.m
@@ -5,11 +5,17 @@ function [dists] = all_pts_to_line(rest_of_data, v1, v2)
 % v1 is first point used to calculate m and b
 % v2 is second point used to calculate m and b
 % dists output vector of distances
+if ~isrow(v1)
+    v1 = v1';
+end
+if ~isrow(v2)
+    v2 = v2';
+end
 pts = [rest_of_data zeros(length(rest_of_data),1)];
-v1 = [repmat(v1,size(pts,1),1) zeros(length(pts),1)];
-v2 = [repmat(v2,size(pts,1),1) zeros(length(pts),1)];
-a = v1 - v2;
-b = pts - v2;
+v1_big = [repmat(v1,size(pts,1),1) zeros(length(pts),1)];
+v2_big = [repmat(v2,size(pts,1),1) zeros(length(pts),1)];
+a = v1_big - v2_big;
+b = pts - v2_big;
 dists = sqrt(sum(cross(a,b,2).^2,2)) ./ sqrt(sum(a.^2,2));
 end
 
--- ransac_implementation.m
+++ ransac_implementation.m
@@ -1,25 +1,38 @@
 function [best_m, best_b] = ransac_implementation(X_train, Y_train)
-% Parameters setting
-numIterations = 3;
-n_pts_in_model = 2; % linear
-dist_thresh = 1;
-
 % RANSAC 
 [m_train,d_train] = size(X_train);
+ndim = length(d_train);
+
+% Parameters setting
+numIterations = 30;
+n_pts_in_model = ndim + 1; % linear
+dist_thresh = 4;
 
 % Array to store number of inliers, slope, intercept
-metric_data = zeros(numIterations,3); 
+metric_data = zeros(numIterations,ndim+2);
 for k = 1:numIterations
     % 1. Draw sample of n points uniformly
     inds = randi([1 m_train],n_pts_in_model,1);
-    point1 = [X_train(inds(1),:) Y_train(inds(1))];
-    point2 = [X_train(inds(2),:) Y_train(inds(2))];
+    v = zeros(d_train+1,length(inds));
+    for iPt = 1:length(inds)
+        v(:,iPt) = [X_train(inds(iPt),:) Y_train(inds(iPt))]';
+    end
+    %point1 = [X_train(inds(1),:) Y_train(inds(1))];
+    %point2 = [X_train(inds(2),:) Y_train(inds(2))];
     rest_of_data = [X_train Y_train];
     rest_of_data(inds,:) = [];
     % 2. Train set of data
-    [m, b] = fit_a_line(point1, point2);
-    % 3. Compute distances to this line
-    dists = all_pts_to_line(rest_of_data, point1, point2);
+    if ndim == 1
+        [m, b] = fit_a_line(v(:,1), v(:,2));
+        % 3. Compute distances to this line
+        dists = all_pts_to_line(rest_of_data, v(:,1), v(:,2));
+    else
+        x_data = [v(1:ndim,:); ones(1,3)]';
+        y_data = v(ndim+1,:)';
+        m = pinv(x_data'*x_data) * x_data' * y_data;
+        % 3. Compute distances to this line
+        dists = all_pts_to_hyperplanes(rest_of_data, v(:,1), v(:,2), v(:,3));
+    end
     % 4. Compute number of inliers vs. outliers
     num_in = sum(dists <= dist_thresh);
     metric_data(k,:) = [num_in m b];
@@ -27,7 +40,7 @@ end
 
 % Find index with maximum number of inliers
 [~, best_ind] = max(metric_data(:,1));
-best_m = metric_data(best_ind,2);
-best_b = metric_data(best_ind,3);
+best_m = metric_data(best_ind,2:ndim+1);
+best_b = metric_data(best_ind,ndim+2);
 
     
